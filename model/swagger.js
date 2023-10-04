c806f9e09cce12d66296b62c98d46b7bdbe90f1d
nattawut rojchanachan <nattawutroj@gmail.com>
2023-10-04 21:56:18 +0700

    Test env

--- model/swagger.js
+++
@@ -1,6 +0,0 @@
-
-const swaggerUi = require("swagger-ui-express");
-const YAML = require('yamljs');
-const path = require('path');
-const swaggerDocument = YAML.load(path.join(__dirname, 'api-src.yaml'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
