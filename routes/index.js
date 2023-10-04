99077f8a1295debc2c5b74ae9e21c9f431e7a815
nattawut rojchanachan <nattawutroj@gmail.com>
2023-10-04 22:10:15 +0700

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+
 require('dotenv').config();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', key: process.env.DB_HOST });
+  console.log(process.env.DB_HOST);
+  res.render('index', { title: 'Express'});
 });
 
 module.exports = router;
