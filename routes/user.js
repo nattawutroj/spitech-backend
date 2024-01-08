b1ac7396df9731ba49defe10389b070ed9b35b8c
nattawut rojchanachan <nattawutroj@gmail.com>
2024-01-08 16:30:38 +0700

    :alien: Add project build and join functionality

--- routes/user.js
+++ routes/user.js
@@ -63,19 +63,19 @@ app.post('/login', (req, res) => {
 })
 
 app.put('/change', (req, res) => {
-    try{
-    req.body.new_password = cyp.encode(req.body.new_password)
+    try {
+        req.body.new_password = cyp.encode(req.body.new_password)
         if (Object.keys(req.result)[0] == 'id_student') {
             db.changePasswordStd(req, (result) => {
-                result == 422 ? cto.e422(res) : cto.o200(res, {password: "Change password success"})
+                result == 422 ? cto.e422(res) : cto.o200(res, { password: "Change password success" })
             })
         }
         else if (Object.keys(req.result)[0] == 'id_staff') {
             db.changePasswordSaff(req, (result) => {
-                result == 422 ? cto.e422(res) : cto.o200(res, {password: "Change password success"})
+                result == 422 ? cto.e422(res) : cto.o200(res, { password: "Change password success" })
             })
         }
-    } catch(err) {
+    } catch (err) {
         cto.e500(res)
     }
 })
@@ -92,4 +92,62 @@ app.put('/profile', (req, res) => {
         result == 422 ? cto.e422(res) : cto.o200(res, result)
     })
 })
+
+app.post('/build', (req, res) => {
+    db.selectSemester((result) => {
+        db.projectCodeBuild(result[0].id_semester, (rowCount) => {
+            debug(rowCount)
+            var projectCode = result[0].year[2] + result[0].year[3] + result[0].semester + (rowCount + 1).pad(5)
+            var build = {
+                id_project: projectCode,
+                project_title_th: req.body.project_title_th,
+                project_title_en: req.body.project_title_en,
+                project_study_title_th: req.body.project_study_title_th,
+                project_study_title_en: req.body.project_study_title_en,
+                id_semester: result[0].id_semester,
+                id_student: req.result.id_student
+            }
+            debug(build)
+            db.projectBuild(build, (result) => {
+                debug(result)
+                result == 422 ? cto.e422(res) : cto.o200(res)
+            })
+        })
+
+    })
+})
+
+app.post('/join', (req, res) => {
+    var build = {
+        id_project: req.body.id_project,
+        id_student: req.result.id_student
+    }
+    db.projectJoin(build, (result) => {
+        debug(result)
+        result == 422 ? cto.e422(res) : result == 400 ? cto.e400(res) : cto.o200(res)
+    })
+})
+
+app.get('/join', (req, res) => {
+    var build = {
+        id_project: req.body.id_project,
+        id_student: req.result.id_student
+    }
+    db.projectCheck(build, (result) => {
+        debug(result)
+        result == 422 ? cto.e422(res) : result == 400 ? cto.e400(res) : cto.o200(res,result.rows)
+    })
+})
 module.exports = app;
+
+Number.prototype.pad = function (size) {
+    var s = String(this);
+    while (s.length < (size || 2)) { s = "0" + s; }
+    return s;
+}
+
+function debug(x) {
+    console.log("\n:::::::::::::::::::::::: {{ DEBUG }} :::::::::::::::::::::::: ")
+    console.log(x)
+    console.log("::::::::::::::::::::::::: {{ END }} ::::::::::::::::::::::::: \n")
+}
